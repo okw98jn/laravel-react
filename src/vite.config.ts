11f69a9305e3d9994bf27a3c72fd35d2864e4eaa
okw98jn <lcmc9vgool@gmail.com>
2025-03-02 08:12:10 +0000

    フォーマット

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/ts/main.tsx'],
-            refresh: true,
-        }),
-        tailwindcss(),
-        react(),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/ts/main.tsx'],
+      refresh: true,
+    }),
+    tailwindcss(),
+    react(),
+  ],
 });
