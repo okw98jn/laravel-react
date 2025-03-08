6a7be608ad0a2763118f2ab1cf523928186a404a
okw98jn <lcmc9vgool@gmail.com>
2025-03-08 15:03:43 +0000

    フォームコンポーネントとバリデーションのサポートを追加
    - React Hook Formとその関連パッケージをプロジェクトに統合
    - 再利用可能な入力コンポーネントを作成
    - UIコンポーネント（Form、Input、Label）を追加
    - フォームバリデーションのための基本的な構造を実装

---
+++ src/resources/ts/components/form/input.tsx
@@ -0,0 +1,35 @@
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input as InputComponent } from '@/components/ui/input';
+import type { InputHTMLAttributes } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+interface Props<S> extends InputHTMLAttributes<HTMLInputElement> {
+  name: keyof S & string;
+  label?: string;
+}
+
+export function Input<S>({ name, label, ...props }: Props<S>) {
+  const form = useFormContext();
+
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          {label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+          <FormControl>
+            <InputComponent id={name} {...field} {...props} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
