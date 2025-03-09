8076a09baba3d3dd7f7d54d3cc4e710d04396b8f
okw98jn <lcmc9vgool@gmail.com>
2025-03-09 13:50:40 +0000

    仮にログアウト処理を作成

--- src/resources/ts/routes/admin/_authenticated/index.tsx
+++ src/resources/ts/routes/admin/_authenticated/index.tsx
@@ -1,9 +1,29 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button';
+import { api } from '@/lib/api-client';
+import { useMutation } from '@tanstack/react-query';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { toast } from 'sonner';
 
 export const Route = createFileRoute('/admin/_authenticated/')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/admin/_authenticated/"!</div>
+  const navigate = useNavigate();
+
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      return api.post('/admin/logout');
+    },
+    onSuccess: () => {
+      navigate({ to: '/admin/login' });
+      toast.success('ログアウトしました。');
+    },
+  });
+
+  return (
+    <div>
+      <Button onClick={() => mutate()}>Logout</Button>
+    </div>
+  );
 }
