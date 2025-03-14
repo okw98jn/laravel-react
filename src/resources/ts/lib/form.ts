22a8bc0aa2cae800d75179a0aed1a0dd1942917a
okw98jn <lcmc9vgool@gmail.com>
2025-03-14 02:17:41 +0000

    APIエラーレスポンスの型を`ValidationErrorResponse`から`ApiErrorResponse`に変更し、エラーメッセージの取得方法を修正

--- src/resources/ts/lib/form.ts
+++ src/resources/ts/lib/form.ts
@@ -1,9 +1,9 @@
-import type { ValidationErrorResponse } from '@/types/api';
+import type { ApiErrorResponse } from '@/types/api';
 import type { AxiosError } from 'axios';
 import type { FieldValues, Path, UseFormSetError } from 'react-hook-form';
 
 /**
- * APIのバリデーションエラーをフォームのエラーに変換する
+ * APIのバリデーションエラーをフォームエラーに変換する
  *
  * @param error - APIのエラー
  * @param setError - フォームのエラーを設定する関数
@@ -12,7 +12,7 @@ export const setApiValidationError = <T extends FieldValues>(
   error: AxiosError,
   setError: UseFormSetError<T>,
 ): void => {
-  const errors = (error.response?.data as ValidationErrorResponse)?.body;
+  const errors = (error.response?.data as ApiErrorResponse)?.error;
 
   if (errors) {
     for (const [field, message] of Object.entries(errors)) {
