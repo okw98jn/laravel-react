9d2124586189596cabae0b3e711dbc58d5b505ba
okw98jn <lcmc9vgool@gmail.com>
2025-03-14 17:09:08 +0000

    クエリのデフォルト設定を拡張し、エラーハンドリングのリトライロジックを追加。400番台のエラーに対してリトライを無効化し、コードの堅牢性を向上。

--- src/resources/ts/lib/query.ts
+++ src/resources/ts/lib/query.ts
@@ -1,7 +1,26 @@
+import { HTTP_STATUS } from '@/constants/http-status';
 import { type DefaultOptions, QueryClient } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
 
-const defaultQueryConfig = {} satisfies DefaultOptions;
+const defaultQueryConfig = {
+  queries: {
+    retry(failureCount, error) {
+      const RETRY_COUNT = 3;
+
+      if (failureCount === RETRY_COUNT) return false;
+
+      // 400番台のエラーはリトライしない
+      if (
+        error.response?.status &&
+        error.response.status < HTTP_STATUS.INTERNAL_SERVER_ERROR
+      ) {
+        return false;
+      }
+
+      return true;
+    },
+  },
+} satisfies DefaultOptions;
 
 export const queryClient = new QueryClient({
   defaultOptions: defaultQueryConfig,
