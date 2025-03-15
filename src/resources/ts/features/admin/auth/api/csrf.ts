7c7c6540357ecebc41487af049ebac2ff1948698
okw98jn <lcmc9vgool@gmail.com>
2025-03-15 15:21:25 +0000

    csrf.tsファイルにコメントを追加し、axiosを使用してXSRF-TOKENを取得する処理の説明を明確化。

--- src/resources/ts/features/admin/auth/api/csrf.ts
+++ src/resources/ts/features/admin/auth/api/csrf.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export async function getCsrfCookie() {
+  // APIを叩くだけでいいのでaxiosを使用
+  // CookieからXSRF-TOKENを取得し、X-XSRF-TOKENヘッダへセットしてくれる
   await axios.get('/sanctum/csrf-cookie');
 }
