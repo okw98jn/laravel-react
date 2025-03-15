ff013c9f770f84074dc0738685613f8064591a50
okw98jn <lcmc9vgool@gmail.com>
2025-03-15 16:06:24 +0000

    ルートコンポーネントにリダイレクト機能を追加し、未認証ユーザーをログインページに誘導するように変更。コンポーネントの内容を簡素化し、不要な要素を削除。

--- src/resources/ts/routes/index.tsx
+++ src/resources/ts/routes/index.tsx
@@ -1,15 +1,12 @@
-import { Button } from '@/components/ui/button';
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
+  beforeLoad: async () => {
+    throw redirect({ to: '/admin/login' });
+  },
 });
 
 function RouteComponent() {
-  return (
-    <div>
-      Hello "/"!
-      <Button>Button</Button>
-    </div>
-  );
+  return null;
 }
