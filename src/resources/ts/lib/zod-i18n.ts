a3013ca122c2956f20da0267ed821052dcd0d7cf
okw98jn <lcmc9vgool@gmail.com>
2025-03-20 06:42:41 +0000

    zodエラーメッセージの日本語化を強化し、属性の翻訳を追加。古い翻訳ファイルを削除し、新しい構造に移行。

--- src/resources/ts/lib/zod-i18n.ts
+++ src/resources/ts/lib/zod-i18n.ts
@@ -1,15 +1,18 @@
-import translation from '@/locales/ja.json';
+import attributes from '@/locales/ja/attributes.json';
+import zod from '@/locales/ja/zod.json';
 import i18next from 'i18next';
 import { z } from 'zod';
-import { zodI18nMap } from 'zod-i18n-map';
+import { makeZodI18nMap } from 'zod-i18n-map';
 
 // バリデーションメッセージを日本語化
 i18next.init({
   lng: 'ja',
   resources: {
-    ja: { zod: translation },
+    ja: {
+      zod: zod,
+      attributes: attributes,
+    },
   },
 });
-z.setErrorMap(zodI18nMap);
 
-export { z };
+z.setErrorMap(makeZodI18nMap({ ns: ['zod', 'attributes'] }));
