c76200af3a8f8ecab1dc7d938b7b3a2b698ac0f1
okw98jn <lcmc9vgool@gmail.com>
2025-03-22 06:45:11 +0000

    アプリケーションのミドルウェアからGuestApiを削除し、状態管理の設定を更新しました。

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Exceptions\Handler;
-use App\Http\Middleware\GuestApi;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,7 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
-        $middleware->append(GuestApi::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptionHandler = new Handler();
