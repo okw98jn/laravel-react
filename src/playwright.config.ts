bbe27a064762e229fb2d9ea8ebdf5afd8bddbbbb
okw98jn <lcmc9vgool@gmail.com>
2025-03-30 00:04:20 +0000

    Playwrightの設定ファイルで、CI環境に応じてbaseURLを動的に設定するように変更しました。これにより、テスト環境におけるURLの柔軟性が向上します。

--- src/playwright.config.ts
+++ src/playwright.config.ts
@@ -29,7 +29,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://laravel-web',
+    // @ts-ignore
+    baseURL: process.env.CI ? 'http://127.0.0.1:8000' : 'http://laravel-web',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
