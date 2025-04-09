2459edb505f327b21a7957f9ce3e574e91b5e935
okw98jn <lcmc9vgool@gmail.com>
2025-04-09 23:51:20 +0900

    都道府県を表すTypeScript型とレコードを新規作成し、各都道府県のラベルと値を設定しました。

---
+++ src/types/prefecture.ts
@@ -0,0 +1,195 @@
+export type Prefecture = {
+  label: string;
+  value: number;
+};
+
+export const prefectures: Record<string, Prefecture> = {
+  hokkaido: {
+    label: "北海道",
+    value: 1,
+  },
+  aomori: {
+    label: "青森県",
+    value: 2,
+  },
+  iwate: {
+    label: "岩手県",
+    value: 3,
+  },
+  miyagi: {
+    label: "宮城県",
+    value: 4,
+  },
+  akita: {
+    label: "秋田県",
+    value: 5,
+  },
+  yamagata: {
+    label: "山形県",
+    value: 6,
+  },
+  fukushima: {
+    label: "福島県",
+    value: 7,
+  },
+  ibaraki: {
+    label: "茨城県",
+    value: 8,
+  },
+  tochigi: {
+    label: "栃木県",
+    value: 9,
+  },
+  gunma: {
+    label: "群馬県",
+    value: 10,
+  },
+  saitama: {
+    label: "埼玉県",
+    value: 11,
+  },
+  chiba: {
+    label: "千葉県",
+    value: 12,
+  },
+  tokyo: {
+    label: "東京都",
+    value: 13,
+  },
+  kanagawa: {
+    label: "神奈川県",
+    value: 14,
+  },
+  niigata: {
+    label: "新潟県",
+    value: 15,
+  },
+  toyama: {
+    label: "富山県",
+    value: 16,
+  },
+  ishikawa: {
+    label: "石川県",
+    value: 17,
+  },
+  fukui: {
+    label: "福井県",
+    value: 18,
+  },
+  yamanashi: {
+    label: "山梨県",
+    value: 19,
+  },
+  nagano: {
+    label: "長野県",
+    value: 20,
+  },
+  gifu: {
+    label: "岐阜県",
+    value: 21,
+  },
+  shizuoka: {
+    label: "静岡県",
+    value: 22,
+  },
+  aichi: {
+    label: "愛知県",
+    value: 23,
+  },
+  mie: {
+    label: "三重県",
+    value: 24,
+  },
+  shiga: {
+    label: "滋賀県",
+    value: 25,
+  },
+  kyoto: {
+    label: "京都府",
+    value: 26,
+  },
+  osaka: {
+    label: "大阪府",
+    value: 27,
+  },
+  hyogo: {
+    label: "兵庫県",
+    value: 28,
+  },
+  nara: {
+    label: "奈良県",
+    value: 29,
+  },
+  wakayama: {
+    label: "和歌山県",
+    value: 30,
+  },
+  tottori: {
+    label: "鳥取県",
+    value: 31,
+  },
+  shimane: {
+    label: "島根県",
+    value: 32,
+  },
+  okayama: {
+    label: "岡山県",
+    value: 33,
+  },
+  hiroshima: {
+    label: "広島県",
+    value: 34,
+  },
+  yamaguchi: {
+    label: "山口県",
+    value: 35,
+  },
+  tokushima: {
+    label: "徳島県",
+    value: 36,
+  },
+  kagawa: {
+    label: "香川県",
+    value: 37,
+  },
+  ehime: {
+    label: "愛媛県",
+    value: 38,
+  },
+  kochi: {
+    label: "高知県",
+    value: 39,
+  },
+  fukuoka: {
+    label: "福岡県",
+    value: 40,
+  },
+  saga: {
+    label: "佐賀県",
+    value: 41,
+  },
+  nagasaki: {
+    label: "長崎県",
+    value: 42,
+  },
+  kumamoto: {
+    label: "熊本県",
+    value: 43,
+  },
+  oita: {
+    label: "大分県",
+    value: 44,
+  },
+  miyazaki: {
+    label: "宮崎県",
+    value: 45,
+  },
+  kagoshima: {
+    label: "鹿児島県",
+    value: 46,
+  },
+  okinawa: {
+    label: "沖縄県",
+    value: 47,
+  },
+};
