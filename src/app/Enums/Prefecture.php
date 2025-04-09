7f8b3279a1830f11fe2d1dda8d55e2f8628c4228
okw98jn <lcmc9vgool@gmail.com>
2025-04-09 23:49:04 +0900

    都道府県を表すEnumとTypeScript型を追加しました。PHPでのEnum定義と、TypeScriptでの都道府県情報を持つオブジェクトのレコードを作成し、各都道府県のラベルと値を設定しました。

---
+++ src/app/Enums/Prefecture.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Enums;
+
+enum Prefecture: int
+{
+    case Hokkaido = 1;
+    case Aomori = 2;
+    case Iwate = 3;
+    case Miyagi = 4;
+    case Akita = 5;
+    case Yamagata = 6;
+    case Fukushima = 7;
+    case Ibaraki = 8;
+    case Tochigi = 9;
+    case Gunma = 10;
+    case Saitama = 11;
+    case Chiba = 12;
+    case Tokyo = 13;
+    case Kanagawa = 14;
+    case Niigata = 15;
+    case Toyama = 16;
+    case Ishikawa = 17;
+    case Fukui = 18;
+    case Yamanashi = 19;
+    case Nagano = 20;
+    case Gifu = 21;
+    case Shizuoka = 22;
+    case Aichi = 23;
+    case Mie = 24;
+    case Shiga = 25;
+    case Kyoto = 26;
+    case Osaka = 27;
+    case Hyogo = 28;
+    case Nara = 29;
+    case Wakayama = 30;
+    case Tottori = 31;
+    case Shimane = 32;
+    case Okayama = 33;
+    case Hiroshima = 34;
+    case Yamaguchi = 35;
+    case Tokushima = 36;
+    case Kagawa = 37;
+    case Ehime = 38;
+    case Kochi = 39;
+    case Fukuoka = 40;
+    case Saga = 41;
+    case Nagasaki = 42;
+    case Kumamoto = 43;
+    case Oita = 44;
+    case Miyazaki = 45;
+    case Kagoshima = 46;
+    case Okinawa = 47;
+}
